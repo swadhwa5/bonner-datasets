4dcfb0d7358c682a212a45ddd79eb5f76974d2fd
Raj Magesh Gauthaman <raj@raj-magesh.org>
2022-07-16 19:10:50 -0400

    fix: switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
-requirements = (
-    "requests",
-    "boto3",
-    "numpy",
-    "pandas",
-    "xarray",
-    "scipy",
-    "nipy",
-    "h5py",
-    # "brainio", ## TODO add github path
-)
-
-setup(
-    name="bonner-datasets",
-    version="0.1.0",
-    packages=find_namespace_packages(),
-    install_requires=requirements,
-)
+setup()
