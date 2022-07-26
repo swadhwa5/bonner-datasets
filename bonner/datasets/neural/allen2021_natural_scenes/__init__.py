01bf9fb3e52840a18e49db4489d44076198157b3
Raj Magesh Gauthaman <raj.magesh@protonmail.com>
2022-07-26 13:52:04 -0400

    .

--- bonner/datasets/neural/allen2021_natural_scenes/__init__.py
+++ bonner/datasets/neural/allen2021_natural_scenes/__init__.py
@@ -1,6 +1,9 @@
 __all__ = [
     "IDENTIFIER",
     "N_SUBJECTS",
+    "N_SESSIONS",
+    "N_SESSIONS_HELD_OUT",
+    "N_TRIALS_PER_SESSION",
     "compute_nc",
     "average_across_reps",
     "get_shared_stimulus_ids",
@@ -10,6 +13,7 @@ __all__ = [
 from ._utils import (
     IDENTIFIER,
     N_SUBJECTS,
+    N_SESSIONS,
     compute_nc,
     average_across_reps,
     get_shared_stimulus_ids,
--- bonner/datasets/neural/bonner2021_object2vec/__init__.py
+++ bonner/datasets/neural/bonner2021_object2vec/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["IDENTIFIER", "package"]
+__all__ = ["IDENTIFIER", "N_SUBJECTS", "BRAIN_DIMENSIONS", "ROIS", "package"]
 
-from ._utils import IDENTIFIER
+from ._utils import IDENTIFIER, N_SUBJECTS, BRAIN_DIMENSIONS, ROIS
 from ._package import package
