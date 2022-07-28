bf8a61c2b9963ecc1f913ca4438a252aab4e6e05
Raj Magesh Gauthaman <raj.magesh@protonmail.com>
2022-07-27 22:21:41 -0400

    .

---
+++ bonner/datasets/__init__.py
@@ -0,0 +1,6 @@
+__all__ = [
+    "load_data_assembly",
+    "load_stimulus_set",
+]
+
+from ._brainio import load_data_assembly, load_stimulus_set
--- bonner/datasets/neural/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = [
-    "load_data_assembly",
-    "load_stimulus_set",
-]
-
-from ._brainio import load_data_assembly, load_stimulus_set
